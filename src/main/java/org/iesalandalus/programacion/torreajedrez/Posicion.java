432efa481c46f380853e9d86de4cd0868f2ebeb2
Luis <lmr.87@protonmail.ch>
2021-11-10 00:11:36 +0100

    Punto 6
    Creado los métodos "get" y "set" de la clase "Posicion", con las restricciones indicadas y el manejo
    de las excepciones.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,4 +3,36 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Posicion {
     private int fila;
     private char columna;
+
+    /** setter fila */
+    private void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+			/*Hubiera creado una constante para el texto del error, pero dejo un literal
+			  para ajustarme a la definicion de la clase */
+            throw new IllegalArgumentException("ERROR: Fila no válida.");
+        } else {
+            this.fila = fila;
+        }
+    }
+
+    /** getter fila */
+    public int getFila() {
+        return fila;
+    }
+
+    /** setter columna */
+    private void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+			/*Hubiera creado una constante para el texto del error, pero dejo un literal
+			  para ajustarme a la definicion de la clase */
+            throw new IllegalArgumentException("ERROR: Columna no válida.");
+        } else {
+            this.columna = columna;
+        }
+    }
+
+    /** getter columna */
+    public char getColumna() {
+        return columna;
+    }
 }
