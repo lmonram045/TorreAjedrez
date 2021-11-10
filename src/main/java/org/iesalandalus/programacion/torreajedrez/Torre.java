c9e0d573fbeba61ef93a174f4c894c7493dd0485
Luis <lmr.87@protonmail.ch>
2021-11-10 12:59:43 +0100

    Punto 11
    Creada la clase "Torre" y los atributos "color" y "posicion" del tipo adecuados.

---
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.torreajedrez;
+
+public class Torre {
+    // Como no aparecen estos atributos en el diagrama, presupongo que son privados.
+    private Color color;
+    private Posicion posicion;
+}
