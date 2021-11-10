c09f12e078457fd228213d4fef00d6bbb2451ce9
Luis <lmr.87@protonmail.ch>
2021-11-10 01:48:12 +0100

    Commit para sincronizar con el portatil.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import java.util.Objects;
+
 public class Posicion {
     private int fila;
     private char columna;
@@ -49,5 +51,23 @@ public class Posicion {
     /** getter columna  */
     public char getColumna() {
         return columna;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return fila == posicion.fila && columna == posicion.columna;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fila, columna);
+    }
+
+    @Override
+    public String toString() {
+        return "fila=" + fila + ", columna=" + columna;
     }
 }
