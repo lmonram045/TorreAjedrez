74d6cf78f11b16bfafe7d86c0e4d855ce0ad6ded
Luis <lmr.87@protonmail.ch>
2021-11-11 00:15:35 +0100

    Punto 16
    Creado el método mover, que acepta como parámetro una dirección y la cantidad de pasos a mover, lo
    más probable es que no esté bien, pero igualmente seguiré y cuando termine la clase, comprobaré según
    los errores que me salgan en los tests.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Torre {
     // Como no aparecen estos atributos en el diagrama, presupongo que son privados.
     private Color color;
@@ -41,6 +43,82 @@ public class Torre {
             posicion = new Posicion(8, columna);
         }
         this.color = color;
+    }
+    /** Método mover */
+    public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+        if (direccion == null) {
+            throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+        }
+        if (pasos < 1) {
+            throw new IllegalArgumentException("ERROR: El número de pasos debe ser positivo.");
+        }
+
+        Posicion posicionDestino;
+        int filaDestino;
+        char columnaDestino;
+        try {
+            switch (direccion) {
+                case ARRIBA:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = this.posicion.getColumna();
+
+                    for (int i = 1; i <= pasos; i++) {
+                        columnaDestino++;
+                    }
+                    posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    break;
+                case ABAJO:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = this.posicion.getColumna();
+
+                    for (int i = 1; i <= pasos; i++) {
+                        columnaDestino--;
+                    }
+                    posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    break;
+                case DERECHA:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = this.posicion.getColumna();
+
+                    for (int i = 1; i <= pasos; i++) {
+                        filaDestino++;
+                    }
+                    posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    break;
+                case IZQUIERDA:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = this.posicion.getColumna();
+
+                    for (int i = 1; i <= pasos; i++) {
+                        filaDestino--;
+                    }
+                    posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    break;
+                case ENROQUE_CORTO:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = 'f';
+                    if (filaDestino == 1 || filaDestino == 8) {
+                        posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    } else {
+                        throw new OperationNotSupportedException("ERROR: Movimiento de enroque no válido.");
+                    }
+                    break;
+                case ENROQUE_LARGO:
+                    filaDestino = this.posicion.getFila();
+                    columnaDestino = 'd';
+                    if (filaDestino == 1 || filaDestino == 8) {
+                        posicionDestino = new Posicion(filaDestino, columnaDestino);
+                    } else {
+                        throw new OperationNotSupportedException("ERROR: Movimiento de enroque no válido.");
+                    }
+                    break;
+                default:
+                    throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+            }
+        } catch (IllegalArgumentException e) {
+            throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+        }
+        this.setPosicion(posicionDestino);
     }
 
     // ------------------------ Getters y Setters -----------------------------------------------------------------
