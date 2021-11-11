bf6570a3c101a70468e74088f707cfacc03cf0f0
Luis <lmr.87@protonmail.ch>
2021-11-11 02:53:32 +0100

    Punto 20.i
    Creado el método mostrarTorre en la clase MainApp

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -2,6 +2,16 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
 
+	private static Torre torre = null;
+
+	private static void mostrarTorre() {
+		if (torre != null) {
+			System.out.println("Torre: " + torre);
+		} else {
+			System.out.println("Aún no se ha creado ninguna torre.");
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("kk");
 	}
